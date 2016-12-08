7a2968b45fa16f79aa5776bcf89f8bf3900e8bd2
bjork24 <dan.chilton@gmail.com>
2016-12-08 10:33:57 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  plugins: [
+    'import',
+    'html',
+    'promise'
+  ],
+  rules: {
+    'array-bracket-spacing' : 2,
+    'block-spacing': [1, 'always'],
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
+    camelcase: [2, { properties: 'never' }],
+    'comma-spacing': [2, {before: false, after: true}],
+    'comma-style': [2, 'last'],
+    'consistent-this': [2, 'self'],
+    curly: [2, 'multi-line'],
+    'dot-notation' : 1,
+    'eol-last': 2,
+    'guard-for-in': 0,
+    indent: [2, 2],
+    'no-bitwise' : 2,
+    'no-console' : 0,
+    'no-eval' : 2,
+    'no-lone-blocks' : 1,
+    'no-lonely-if': 1,
+    'no-loop-func': 1,
+    'no-multi-str' : 1,
+    'no-multiple-empty-lines': 1,
+    'no-native-reassign' : 2,
+    'no-nested-ternary': 2,
+    'no-new-func' : 1,
+    'no-new-object': 1,
+    'no-new-wrappers': 1,
+    'no-plusplus': 1,
+    'no-spaced-func': 1,
+    'no-trailing-spaces' : 2,
+    'no-unneeded-ternary': 1,
+    'no-use-before-define' : 1,
+    'no-useless-call': 1,
+    'object-curly-spacing': [1, 'always'],
+    quotes: [2, single, 'avoid-escape'],
+    semi: 1,
+    'space-before-blocks': 2,
+    'spaced-comment': 1
+  }
+}
